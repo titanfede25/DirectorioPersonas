6c34ac9bacf53ae339d10dfdc9244fb4960981f4
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-30 12:07:03 -0300

    terminado

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,15 +4,17 @@ export default function COntact() {
       <div className="TituloForm">
       <u>Contactate con nosotros!</u>  
       </div>
+      
       <form >   
+          <br></br><br></br>
           <input type="text"  placeholder="Nombre"  className="u-full-width"  /><br></br><br></br>
           <input type="text"  placeholder="Apellido"  className="u-full-width" /><br></br><br></br>
           <input type="number" placeholder="Edad" className="u-full-width" /><br></br><br></br>
           <input type="text" placeholder="Email"  className="u-full-width" />
           <br></br><br></br>
-          
+          <div className="pixel2">Enviar</div>     
       </form>
-      <div className="ContenedorBoton"><div className="pixel2">Enviar</div></div>
+     
     </>
   );
 }
